cd1eb2194e392473b2855ea3b0a1011f912153b2
akaBaytar <burak.bilgili@icloud.com>
2023-09-12 15:48:52 +0300

    added spinner animation

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,3 +9,16 @@ mobileMenu();
 sidebar();
 
 setData();
+
+// spinner
+const spinner = document.querySelector(".spinner-wrapper");
+const sections = document.querySelectorAll(
+  ".trend-wrapper, .now-showing-wrapper, .in-the-mood-wrapper, .leaving-soon-wrapper, .cultures-wrapper"
+);
+
+sections.forEach((section) => (section.style.display = "none"));
+
+setTimeout(() => {
+  sections.forEach((section) => (section.style.display = "block"));
+  spinner.remove();
+}, 1000);
